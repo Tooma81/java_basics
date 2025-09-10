8932ecc0bc92e8140c5d700b4cf123d289a2b660
toomas <toomas.elmet@ir.ee>
2025-09-10 09:36:04 +0300

    Varying Variables

---
+++ varying_variables.java
@@ -0,0 +1,12 @@
+public class varying_variables {
+    public static void main(String[] args) {
+        int chickens = 3;
+        double bacon = 5.5;
+        String tractor = "There is none!";
+        
+        System.out.println(
+            "Chickens:\n" + chickens + "\nBacon (kg):\n" + bacon + "\nA tractor:\n" + tractor + 
+            "\n\nIn a nutshell:\n" + chickens + "\n" + bacon + "\n" + tractor
+        );
+    }
+}
