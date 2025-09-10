84dadc196178d01a9db19b17215f7356ff103d75
toomas <toomas.elmet@ir.ee>
2025-09-10 13:48:14 +0300

    Login loop

--- Usernames.java
+++ Usernames.java
@@ -4,18 +4,23 @@ public class Usernames {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
-        System.out.print("Type your username: ");
-        String username = reader.nextLine();
-        System.out.print("Type your password: ");
-        String password = reader.nextLine();
+        while (true) {
+            System.out.print("Type your username: ");
+            String username = reader.nextLine();
+            System.out.print("Type your password: ");
+            String password = reader.nextLine();
 
-        if (
-                (username.equals("alex") && password.equals("mightyducks")) || 
-                (username.equals("emily") && password.equals("cat"))
+            if (
+                    (username.equals("alex") && password.equals("mightyducks")) || 
+                    (username.equals("emily") && password.equals("cat"))
             ) {
-            System.out.print("You are now logged into the system!");
-        } else {
-            System.out.print("Your username or password was invalid!");
-        }
+                break;
+            } else {
+                System.out.println("Your username or password was invalid!\n");
+            }
+        } 
+        System.out.print("You are now logged into the system!");  
     }
 }
+
+
